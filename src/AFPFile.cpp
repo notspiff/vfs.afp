e2ebac75b15aa91cfa2b1cc9ff1918b63948a316
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2013-11-28 18:00:16 +0100

    s

--- src/AFPFile.cpp
+++ src/AFPFile.cpp
@@ -534,7 +534,7 @@ void* OpenForWrite(const char* url, const char* hostname,
   AFPContext* result = new AFPContext;
   result->pAfpVol = CAFPConnection::Get().GetVolume();
 
-  std::string strPath = CAfpConnection::Get().GetPath(url);
+  std::string strPath = CAFPConnection::Get().GetPath(url);
 
   if (bOverWrite)
   {
